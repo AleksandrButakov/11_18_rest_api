545bec0406548147752ecc39b1745ae4e2d3b85e
Aleksandr Butakov <anbngm@gmail.com>
2022-04-19 10:46:40 +0500

    Init commit

--- src/test/java/TestAPI.java
+++ src/test/java/TestAPI.java
@@ -87,37 +87,5 @@ public class TestAPI {
 
     }
 
-    @Test
-    @DisplayName("03_addToCartTest33")
-    void addToCartTest33() {
-        Integer cartSize = 0;
-
-        ValidatableResponse response = given()
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie("Nop.customer=3d26c045-d2eb-4a90-ae9f-fae62387cda0;")
-                .body("product_attribute_72_5_18=53" +
-                        "&product_attribute_72_6_19=54" +
-                        "&product_attribute_72_3_20=57" +
-                        "&addtocart_72.EnteredQuantity=1")
-                .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
-                .then()
-                .log().all()  // команда выводит весь контент в чат
-                .statusCode(200)
-                .body("success", is(true))
-                .body("message", is("The product has been added to your \u003ca href=\"/cart\"\u003eshopping cart\u003c/a\u003e"));
-
-        // assertThat(response.extract().path("updatetopcartsectionhtml").toString())
-//                .body("updatetopcartsectionhtml", is("(24)"))
-//                .extract().response().asString();
-
-        System.out.println();
-        System.out.println("Full answer:");
-        System.out.println(response);
-    }
-
-/*
-    Пример рабочего теста:
- */
 
 }
